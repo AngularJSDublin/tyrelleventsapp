5ee525b0ec1b2c06cf0ae356d77dc4d262c4ed84
adisbanda1 <adisbanda1@gmail.com>
2015-06-13 12:51:44 +0100

    Aborting

--- app/app.js
+++ app/app.js
@@ -2,14 +2,4 @@
 
 (function(){
 	var app = angular.module('eventsApp', []);
-
-	//controller as syntax
-	app.controller('SampleController', function(){
-		this.welcome = 'Welcome to the first AngularJS workshop.'
-	});
-
-	app.controller("SecondController", function($scope){
-		$scope.welcomeAgain = 'By the end of the workshops you will build the Angular events app and hopefully understand the framework on deeper level.'
-	})
-
 })();
