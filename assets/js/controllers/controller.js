255e8edbcc5201f7cf9b123e3eaf64da600447b6
adisbanda1 <adisbanda1@gmail.com>
2015-06-13 13:54:46 +0100

    Controller seperated and working

--- assets/js/controllers/controller.js
+++ assets/js/controllers/controller.js
@@ -1,14 +1,15 @@
 // controller.js
 (function(){
-	angular.module('eventsController', []);
+	angular.module('eventsController', [])
 
 	//controller as syntax
 	.controller('SampleController', function(){
 		this.welcome = 'Welcome to the first AngularJS workshop.'
-	});
+	})
 
 	.controller("SecondController", function($scope){
 		$scope.welcomeAgain = 'By the end of the workshops you will build the Angular events app and hopefully understand the framework on deeper level.'
 	});
+
 })();
 
