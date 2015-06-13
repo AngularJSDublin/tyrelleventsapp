5a99612320b4f13fad78e02cd7ee8d8dde209f3c
adisbanda1 <adisbanda1@gmail.com>
2015-06-13 14:43:17 +0100

    Added media list style

--- assets/js/controllers/controller.js
+++ assets/js/controllers/controller.js
@@ -6,13 +6,7 @@
 	.controller('mockDataController', ['$scope', '$http', function($scope, $http){
 		$http.get('https://tyrelleventsdb.firebaseio.com/events.json').success(function(data){
 			$scope.mockData = data;
-			$scope.category = data.category;
 		});
 	}])
-
-	.controller("SecondController", function($scope){
-		$scope.welcomeAgain = 'By the end of the workshops you will build the Angular events app and hopefully understand the framework on deeper level.'
-	});
-
 })();
 
