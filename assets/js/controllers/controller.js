fa3ccbbcb6d8d68006172512b8dba55f34cbb86b
adisbanda1 <adisbanda1@gmail.com>
2015-06-13 14:07:39 +0100

    http request working

--- assets/js/controllers/controller.js
+++ assets/js/controllers/controller.js
@@ -3,9 +3,11 @@
 	angular.module('eventsController', [])
 
 	//controller as syntax
-	.controller('SampleController', function(){
-		this.welcome = 'Welcome to the first AngularJS workshop.'
-	})
+	.controller('mockDataController', ['$scope', '$http', function($scope, $http){
+		$http.get('https://tyrelleventsdb.firebaseio.com/events.json').success(function(data){
+			$scope.mockData = data;
+		});
+	}])
 
 	.controller("SecondController", function($scope){
 		$scope.welcomeAgain = 'By the end of the workshops you will build the Angular events app and hopefully understand the framework on deeper level.'
