7ae6c085a07aa916472efd0ab4021471fa7fb4b3
Christian <christian@Christians-MacBook-Pro.local>
2015-06-15 22:09:12 +0100

    add routing

--- app/app.js
+++ app/app.js
@@ -1,6 +1,25 @@
 (function(){
+	
 	var app = angular.module('eventsApp', [
+		'ngRoute',
 		'eventsController'
 	])
+
+	app.config(['$routeProvider',
+	  function($routeProvider) {
+	    $routeProvider.
+	      when('/', {
+	        templateUrl: 'assets/js/templates/events-list.html',
+	        controller: 'mockDataController'
+	      }).
+	      when('/events/:eventId', {
+	        templateUrl: 'assets/js/templates/event-detail.html',
+	        controller: 'eventDetailController'
+	      }).
+	      otherwise({
+	        redirectTo: '/'
+	      });
+	  }]);
+
 })();
 
--- assets/js/controllers/controller.js
+++ assets/js/controllers/controller.js
@@ -1,12 +1,26 @@
-// controller.js
 (function(){
-	angular.module('eventsController', [])
 
-	//controller as syntax
+	var eventsController = angular.module('eventsController', [])
+
+	// Get All Events
 	.controller('mockDataController', ['$scope', '$http', function($scope, $http){
 		$http.get('https://tyrelleventsdb.firebaseio.com/events.json').success(function(data){
 			$scope.mockData = data;
 		});
 	}])
+
+	// Get All Events
+	.controller('singleEventController', ['$scope', '$http', function($scope, $http){
+		$http.get('https://tyrelleventsdb.firebaseio.com/events/'+ $routeParams.eventId +'.json').success(function(data){
+			$scope.eventData = data;
+		});
+	}])
+
+	// Routing
+	.controller('eventDetailController', ['$scope', '$routeParams',
+	  function($scope, $routeParams) {
+	    $scope.eventId = $routeParams.eventId;
+	  }]);
+	
 })();
 
