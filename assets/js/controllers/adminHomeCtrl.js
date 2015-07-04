0c8c0d33c36127f48b317b4a577aa5c4cf91948c
Christian <christian@Christians-MacBook-Pro.local>
2015-07-04 11:32:42 +0100

    fic login auth

--- assets/js/controllers/adminHomeCtrl.js
+++ assets/js/controllers/adminHomeCtrl.js
@@ -1,19 +1,14 @@
 angular.module('eventsApp')
   //Admin Home
-  .controller('adminHomeCtrl', ['$scope', '$http', function ($scope, $http) {
+  .controller('adminHomeCtrl', ['$scope', '$http', 'profile', function($scope,
+    $http, profile) {
 
-    var getToken = localStorage.getItem('firebase:session::tyrelleventsdb');
-    var tokenStorage = JSON.parse(getToken);
+    $scope.profile = profile;
 
-    $http.get('https://tyrelleventsdb.firebaseio.com/users/' + tokenStorage.uid + '.json').success(function (data) {
-      $scope.user = {
-        isAdmin: data.role == 'admin'
-      };
-    });
-
-    $http.get('https://tyrelleventsdb.firebaseio.com/events.json').success(function (data) {
-      $scope.mockData = data;
-    });
+    $http.get('https://tyrelleventsdb.firebaseio.com/events.json').success(
+      function(data) {
+        $scope.mockData = data;
+      });
 
 
   }]);
--- assets/js/controllers/loginCtrl.js
+++ assets/js/controllers/loginCtrl.js
@@ -1,34 +1,39 @@
 angular.module('eventsApp')
-  .controller('loginController', ['$scope', '$http','$location', function ($scope, $http, $location) {
+  .controller('loginController', ['$scope', '$http', '$location', 'profile',
+    function(
+      $scope, $http, $location, profile) {
+
+      // Logging Users In
+      $scope.login = function(email, password) {
+        var ref = new Firebase("https://tyrelleventsdb.firebaseio.com/");
+        ref.authWithPassword({
+          email: email,
+          password: password
+        }, function(error, authData) {
+          if (error) {
+            console.log("Login Failed!", error);
+          } else {
+            console.log("Authenticated successfully with payload:",
+              authData);
 
 
 
-    // Logging Users In
-    $scope.login = function (email, password) {
-      var ref = new Firebase("https://tyrelleventsdb.firebaseio.com/");
-      ref.authWithPassword({
-        email: email,
-        password: password
-      }, function (error, authData) {
-        if (error) {
-          console.log("Login Failed!", error);
-        } else {
-          console.log("Authenticated successfully with payload:", authData);
-          
-          
-          
-          $http.get('https://tyrelleventsdb.firebaseio.com/users/' + authData.uid + '.json').success(function (data) {
-            if(data.role == 'admin'){
-              $location.path('/admin-home');
-            } else{
-              $location.path('/event-list');
-            }
-         
-          });
-        }
-      });
+            $http.get('https://tyrelleventsdb.firebaseio.com/users/' +
+              authData.uid + '.json?' + 'auth=' + authData.token).success(
+              function(data) {
+                angular.extend(profile, data);
+                if (data.role == 'admin') {
+                  $location.path('/admin-home');
+                } else {
+                  $location.path('/event-list');
+                }
+
+              });
+          }
+        });
+      }
+
+
+
     }
-
-
-
-  }]);
+  ]);
